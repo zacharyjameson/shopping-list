0715f29fe43859d6dbe1ad75863939846c9eb79c
Zack Jameson <zacharymjameson@gmail.com>
2021-01-06 20:14:45 -0500

    javascript add userInput code with no other features, just adding in new input text into their own <li>

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-test
+$(function () {
+  $("#js-shopping-list-form").submit((event) => {
+    event.preventDefault();
+    const inputText = $(event.currentTarget).find("#shopping-list-entry");
+    $(".shopping-list").append(
+      <li>
+        <span class="shopping-item">inputText</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+    );
+  });
+});
