d1045408621943fe20e0a180f06ca4d9203f3245
Zack Jameson <zacharymjameson@gmail.com>
2021-01-07 08:32:12 -0500

    progress work on jQuery/JS

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 $(function () {
-  $("#js-shopping-list-form").submit((event) => {
+  $("#js-shopping-list-form").submit(function (event) {
     event.preventDefault();
-    const inputText = $(event.currentTarget).find("#shopping-list-entry");
+    const inputText = $(event).val("#shopping-list-entry");
     $(".shopping-list").append(
-      <li>
-        <span class="shopping-item">inputText</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>
+      '<li>\
+      <span class="shopping-item>' + inputText + '</span>\
+      <div class="shopping-item-controls">\
+        <button class="shopping-item-toggle">\
+          <span class="button-label">check</span>\
+        </button>\
+        <button class="shopping-item-delete">\
+          <span class="button-label">delete</span>\
+        </button>\
+      </div>'
     );
   });
+
+  $(".shopping-list").on("click", ".shopping-item-delete", function () {
+    $(this).closest("li").remove();
+  });
+
+  $(".shopping-list").on("click", ".shopping-item-toggle", function () {
+    $(this).closest('.shopping-item').toggleClass("shopping-item__checked");
+  });
+
 });
