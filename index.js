39a60153bd9c561f1774990163e779bc8e1697f6
Zack Jameson <zacharymjameson@gmail.com>
2021-01-07 11:54:27 -0500

    updated js file with correct code

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 $(function () {
   $("#js-shopping-list-form").submit(function (event) {
     event.preventDefault();
-    const $inputText = $(event).val("#shopping-list-entry");
+    const item = $(this).find('input[name="shopping-list-entry"]').val();
     $(".shopping-list").append(
-      '<li>\
-      <span class="shopping-item>' + $inputText + '</span>\
-      <div class="shopping-item-controls">\
-        <button class="shopping-item-toggle">\
-          <span class="button-label">check</span>\
-        </button>\
-        <button class="shopping-item-delete">\
-          <span class="button-label">delete</span>\
-        </button>\
-      </div>'
+      `<li><span class="shopping-item">${item}</span>
+      <div class="shopping-item-controls">
+      <button class="shopping-item-toggle"><span class="button-label">check</span></button>
+      <button class="shopping-item-delete"><span class="button-label">delete</span></button>
+      </div></li>`
     );
   });
 
@@ -21,7 +16,9 @@ $(function () {
   });
 
   $(".shopping-list").on("click", ".shopping-item-toggle", function () {
-    $(this).closest(".shopping-item").toggleClass("shopping-item__checked");
+    $(this)
+      .closest("li")
+      .find(".shopping-item")
+      .toggleClass("shopping-item__checked");
   });
-
 });
